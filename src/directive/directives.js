7db64dc93f49527e7c79ea9c0f899225b44b3dfd
cwzbooth <cwzbooth@126.com>
2020-07-30 11:15:26 +0800

    first commit

---
+++ src/directive/directives.js
@@ -0,0 +1,13 @@
+import draggable from './module/draggable'
+import clipboard from './module/clipboard'
+import highlight from './module/highlight'
+import has from './module/has-permission'
+
+const directives = {
+  draggable,
+  clipboard,
+  highlight,
+  has
+}
+
+export default directives
